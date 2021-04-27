221d50b465567f778dfc60fe3933e76dd59d287a
Damu2509 <reddivaridamu25091999@gmail.com>
2021-04-27 16:05:24 +0530

    Fetching from database

--- Blog/views.py
+++ Blog/views.py
@@ -2,5 +2,37 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+import pyrebase
+
+config = {
+    'apiKey' : "AIzaSyAtGoWKvmTbbWxseKiFjLqam76x47MHJck",
+    'authDomain' : "project-mtiet-2235c.firebaseapp.com",
+    'databaseURL' : 'https://project-mtiet-2235c-default-rtdb.firebaseio.com/',
+    'projectId' : "project-mtiet-2235c",
+    'storageBucket' : "project-mtiet-2235c.appspot.com",
+    'messagingSenderId' : "289941532910",
+    'appId' : "1:289941532910:web:2d52390e9379e6b75d43ff",
+    'measurementId' : "G-F9XF33FSK2"
+
+}
+firebase = pyrebase.initialize_app(config)
+authe = firebase.auth()
+database = firebase.database()
+
 def home(request):
-    return HttpResponse("<h1> WOW ! Started executing small modules in a sequential manner </h1>")
+
+    name = database.child('Data').child('user').get().val()
+    title = database.child('Data').child('title').get().val()
+
+    content = database.child('Data').child('content').get().val()
+
+    date = database.child('Data').child('date').get().val()
+
+    context =      {
+         'name': name,
+        'title' : title,
+        'cotent' : content,
+        'date' : date
+    }
+
+    return render(request, "Blog/index.html", context)
